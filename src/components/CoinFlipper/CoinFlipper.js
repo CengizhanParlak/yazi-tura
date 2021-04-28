2416274ff3f58d0bc0d48f95270e68a730756ff9
cengizhanParlak <cengizhan.parlak@hotmail.com>
2021-04-28 11:18:48 +0300

    updated CoinFlipper.js

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -3,39 +3,60 @@ import Coin from "../Coin/Coin";
 import "./CoinFlipper.css";
 
 class CoinFlipper extends Component {
-  constructor(props) {
-    super(props);
-    // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
-    // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
-    this.state = {
-      side: "tura",
-      flipping: false,
-    };
-  }
-  handleClick = () => {
-    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
-    this.setState({ flipping: true });
-    // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({ flipping: false }), 1000);
-  };
+   constructor(props) {
+      super(props);
+      // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
+      // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
+      this.state = {
+         side: "tura",
+         flipping: false,
+         tailsCount: 0,
+         headsCount: 0,
+         totalFlip: 0,
+      };
+   }
+   handleClick = () => {
+      this.setState({ totalFlip: this.state.totalFlip + 1 });
+      this.setState({ flipping: this.state.flipping = true })
+      if (Math.random() < 0.5) {
+         this.setState(prevState => {
+            return ({
+               ...prevState,
+               side: 'tura',
+               headsCount: this.state.headsCount + 1,
+            })
+         });
+      }
+      else {
+         this.setState(prevState => {
+            return ({
+               ...prevState,
+               side: 'yazi',
+               tailsCount: this.state.tailsCount + 1,
+            })
+         });
+      }
 
-  render() {
-    return (
-      <div className="CoinFlipper">
-        <h1>Yazı mı Tura mı?</h1>
-        <Coin side={this.state.side} flipping={this.state.flipping} />
-        <button onClick={this.handleClick}>At!</button>
-        <p>
-          Toplam
-          <strong> 5 </strong>
-          atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
-          si yazı geldi.
+      // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+      setTimeout(() => this.setState({ flipping: false }), 1000);
+   };
+
+   render() {
+      return (
+         <div className="CoinFlipper">
+            <h1>Coin Toss</h1>
+            <Coin side={this.state.side} flipping={this.state.flipping} />
+            <button onClick={this.handleClick}>Flip!</button>
+            <p>
+               Tossed
+          <strong> {this.state.totalFlip} </strong>
+          times <br />
+               <strong> {this.state.headsCount} </strong> times was heads and <br />
+               <strong> {this.state.tailsCount} </strong> times was tails.
         </p>
-      </div>
-    );
-  }
+         </div>
+      );
+   }
 }
 
 export default CoinFlipper;
